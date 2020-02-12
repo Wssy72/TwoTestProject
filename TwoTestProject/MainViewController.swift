c19b8e7811793cc11cb69ddb795864098b997431
Sergey <dir@syakovlev.ru>
2020-02-12 18:29:15 +0500

    add snapkit and test constraint

--- TwoTestProject/MainViewController.swift
+++ TwoTestProject/MainViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
-
+    
+    let myView = UIView()
     var myTextView = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.addSubview(myView)
+        
         view.backgroundColor = .gray
         
-        myTextView = UITextView()
         myTextView.frame = CGRect(x: 0, y: 0, width: 150, height: 50)
         myTextView.text = "Введите текст"
+        myTextView.translatesAutoresizingMaskIntoConstraints = false
+        myView.addSubview(myTextView)
+        
+        //debuging
+        myView.snp.makeConstraints { (make) -> Void in
+           make.width.height.equalTo(250)
+           make.center.equalTo(self.view)
+        }
         
     }
 
