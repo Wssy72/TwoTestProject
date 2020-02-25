5c7482b036c5562d1f160fab9186e683898274b7
Sergey <dir@syakovlev.ru>
2020-02-25 11:33:40 +0500

    Update TwoViewController.swift

--- TwoTestProject/TwoViewController.swift
+++ TwoTestProject/TwoViewController.swift
@@ -16,12 +16,8 @@ class TwoViewController: UIViewController {
     override func viewDidLoad() {
     super.viewDidLoad()
     
-    //title = "TwoVC"
     view.backgroundColor = .orange
-        
-        
-        //= UITabBarItem(title: "Title", image: UIImage(named: "open"), selectedImage: UIImage(named: "open"))
-    
+   
     labelTwoVC.frame = CGRect(x: 25, y: 50, width: 350, height: 50)
     labelTwoVC.text = "Добро пожаловать"
     labelTwoVC.textAlignment = .center
